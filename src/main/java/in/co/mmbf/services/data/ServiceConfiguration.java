7bddd4f5787e609a052e8b47468d06752ebff657
Nivas Umapathy <toocoolblue2000@yahoo.com>
2017-03-08 23:30:18 -0500

    added something

--- src/main/java/in/co/mmbf/services/data/ServiceConfiguration.java
+++ src/main/java/in/co/mmbf/services/data/ServiceConfiguration.java
@@ -1,8 +1,17 @@
 package in.co.mmbf.services.data;
 
+import java.util.Properties;
+
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.startup.Tomcat;
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.apache.tomcat.util.descriptor.web.ContextResource;
+import org.hibernate.dialect.MySQLDialect;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -10,6 +19,12 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaSessionFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import ch.qos.logback.core.db.dialect.H2Dialect;
 
 @Configuration
 public class ServiceConfiguration {
@@ -18,8 +33,10 @@ public class ServiceConfiguration {
 	public DataSource postalCodeCsvDataSource() {
 		BasicDataSource postalCodeCsvDataSource = new BasicDataSource();
         postalCodeCsvDataSource.setUrl("jdbc:relique:csv:all_india_pin_code.csv");
-        postalCodeCsvDataSource.setConnectionProperties("columnTypes=string,int,string,string,string,string,string,string,string,string");
+        postalCodeCsvDataSource.setConnectionProperties("columnTypes=string,int,string,string,string,string,string,string,string,string; hibernate.dialect=org.hibernate.dialect.H2Dialect ");
         return postalCodeCsvDataSource;
+
+
 	}
 
 	@Bean
@@ -39,8 +56,58 @@ public class ServiceConfiguration {
 	@Profile("!mock")
 	public DataSource dbDataSource() {
 		BasicDataSource postalCodeCsvDataSource = new BasicDataSource();
-        postalCodeCsvDataSource.setUrl("jdbc:relique:csv:https://data.gov.in/resources/all-india-pincode-directory/download");
-        postalCodeCsvDataSource.setConnectionProperties("columnTypes=string,int,string,string,string,string,string,string,string,string");
+        /*postalCodeCsvDataSource.setUrl("jdbc:relique:csv:all_india_pin_code.csv");
+        postalCodeCsvDataSource.setConnectionProperties("columnTypes=string,int,string,string,string,string,string,string,string,string");*/
         return postalCodeCsvDataSource;
 	}
+
+	@Bean
+	public HibernateJpaVendorAdapter getJpaAdapter() {
+		HibernateJpaVendorAdapter mysqlAdapter = new HibernateJpaVendorAdapter();
+		mysqlAdapter.setDatabase(Database.H2);
+		mysqlAdapter.setDatabasePlatform(H2Dialect.class.getName());
+		mysqlAdapter.setShowSql(true);
+		return mysqlAdapter;
+	}
+
+	@Bean
+	public LocalContainerEntityManagerFactoryBean getLocalContainerEntityManagerFactoryBean() {
+		LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+		entityManagerFactoryBean.setDataSource(postalCodeCsvDataSource());
+		entityManagerFactoryBean.setJpaVendorAdapter(getJpaAdapter());
+		return entityManagerFactoryBean;
+	}
+
+	@Bean
+    public HibernateJpaSessionFactoryBean sessionFactory(EntityManagerFactory emf) {
+         HibernateJpaSessionFactoryBean factory = new HibernateJpaSessionFactoryBean();
+         factory.setEntityManagerFactory(emf);
+         Properties props = new Properties();
+         props.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+         factory.setJpaProperties(props);
+         return factory;
+    }
+
+	/*@Bean
+	public TomcatEmbeddedServletContainerFactory tomcatFactory() {
+	    return new TomcatEmbeddedServletContainerFactory() {
+
+	    	@Override
+	    	protected TomcatEmbeddedServletContainer getTomcatEmbeddedServletContainer(Tomcat tomcat) {
+	    		tomcat.enableNaming();
+	    		return super.getTomcatEmbeddedServletContainer(tomcat);
+	    	}
+
+	    	@Override
+	    	protected void postProcessContext(Context context) {
+	    		ContextResource resource = new ContextResource();
+	            resource.setName("jdbc/myDataSource");
+	            resource.setType(DataSource.class.getName());
+	            resource.setProperty("driverClassName", "your.db.Driver");
+	            resource.setProperty("url", "jdbc:yourDb");
+	            context.getNamingResources().addResource(resource);
+	    	}
+
+	    };
+	}*/
 }
